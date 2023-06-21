7f785d799961f115dd27a8f8228003b43ee245d0
omaffezzolli <joao.l09@aluno.ifsc.edu.br>
2023-06-21 14:39:47 -0300

    ex2

---
+++ academy_pratica_e1/ed1_ap2.dart
@@ -0,0 +1,14 @@
+
+void main() {
+  
+  const nota1 = 7.0;
+  const nota2 = 8.0;
+  const nota3 = 5.3;
+  const nota4 = 9.7;
+  const media = (nota1 + nota2 + nota3 + nota4) / 4;
+  
+  print("A media entre [ $nota1, $nota2, $nota3, $nota4,]= $media");
+  
+  
+    // ...
+}
